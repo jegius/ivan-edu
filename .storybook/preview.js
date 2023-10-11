f851673ba2edb392d27213ff324c93ef74c6aff4
Jhon-Crow <jhamish@mail.ru>
2023-10-11 06:35:24 +0300

    .storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+/** @type { import('@storybook/web-components').Preview } */
+import {
+	ButtonComponent,
+} from '../storybook/src/components/button/button-component';
+
+const preview = {
+	parameters: {
+		actions: {argTypesRegex: '^on[A-Z].*'},
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+	},
+};
+
+export default preview;
+
+
+[
+	ButtonComponent,
+].map((component) => customElements.define(component.name, component));
