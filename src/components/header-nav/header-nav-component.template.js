16d6299a4747d623ee9e34d7b5c38e30dd190e2c
Jhon-Crow <jhamish@mail.ru>
2024-01-08 00:39:33 +0300

    сделал что понял

--- src/components/header-nav/header-nav-component.template.js
+++ src/components/header-nav/header-nav-component.template.js
@@ -3,10 +3,12 @@ import styles from './header-nav-component.styles.js';
 
 export default `
     ${styles}
+<nav>
 <ul class='header__nav'>
 <li class='header__nav-item'><link-component></link-component></li>
 <li class='header__nav-item'><link-component></link-component></li>
 <li class='header__nav-item'><link-component></link-component></li>
 <li class='header__nav-item'><link-component></link-component></li>
 </ul>
+</nav>
 `;
--- src/components/header/header-component.template.js
+++ src/components/header/header-component.template.js
@@ -3,9 +3,9 @@ import styles from './header-component.styles.js';
 
 export default `
     ${styles}
-<ul class='header'>
+<header class='header'>
 <header-logo-component></header-logo-component>
 <header-nav-component></header-nav-component>
 <header-line-wrapper-component></header-line-wrapper-component>
-</ul>
+</header>
 `;
