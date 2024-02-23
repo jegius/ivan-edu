89319cdf1051344d32c7974911cd7f089e0810c9
Jhon-Crow <jhamish@mail.ru>
2024-02-23 16:38:42 +0300

    поправил темплейт

--- src/components/button/button-component.template.js
+++ src/components/button/button-component.template.js
@@ -2,5 +2,5 @@ import styles from './button-component.styles.js';
 
 export default `
     ${styles}
-    <input type='button' class='button button_light' value='Button Component'>
+    <input type='button' class='button button_light _medium _light _active' value='Button Component'>
 `;
