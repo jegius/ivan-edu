8bee90f6c6c0097284ce5c426493c54ff2c0240a
Jhon-Crow <jhamish@mail.ru>
2024-02-23 17:02:15 +0300

    начал линк надо разобраться с api

--- src/components/link/link-component.js
+++ src/components/link/link-component.js
@@ -1,6 +1,23 @@
 import template from './link-component.template.js';
 
+const linkAttributes = {
+	LINK_TEXT: 'text',
+	HREF: 'href',
+};
+
 export class LinkComponent extends HTMLElement {
+	#href;
+	#link;
+
+	#listeners = [
+		[select.bind(this, '.link'), 'click', this.#addEventListeners.bind(this)],
+	];
+
+	#ATTRIBUTES_MAPPING = new Map([
+		[linkAttributes.LINK_TEXT, LinkComponent.#setText],
+		[linkAttributes.HREF, LinkComponent.#setHref],
+	]);
+
 	constructor() {
 		super();
 		this.attachShadow({mode: 'open'});
@@ -10,13 +27,63 @@ export class LinkComponent extends HTMLElement {
 		return 'link-component';
 	}
 
+	static get observedAttributes() {
+		return Object.values(linkAttributes);
+	}
+
+	static #setText(element, newText) {
+		element.innerHTML = newText;
+	}
+
+	static #setHref(element, newHref) {
+		this.#href = newHref;
+		if (element) {
+			element.setAttribute('href', newHref);
+		}
+	}
+
 	connectedCallback() {
 		this.#render();
+		this.#listeners.forEach(addListeners.bind(this));
+
+		for (let attrName of this.constructor.observedAttributes) {
+			if (this.hasAttribute(attrName)) {
+				const attrValue = this.getAttribute(attrName);
+				this.attributeChangedCallback(attrName, null, attrValue);
+			}
+		}
+	}
+
+	#addEventListeners(event) {
+		const element = this.#href !== '#' ? document.querySelector(this.#href) : null;
+
+		if (element) {
+			event.preventDefault();
+		}
+
+	}
+
+	disconnectedCallback() {
+		this.#listeners.forEach(removeListeners);
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (newValue !== oldValue) {
+			const callback = this.#ATTRIBUTES_MAPPING.get(name);
+			this.#selectAndCallIfExist(callback, newValue);
+		}
+	}
+
+	#selectAndCallIfExist(callback, value) {
+		if (this.#link) {
+			callback.call(this, this.#link, value);
+		}
 	}
 
 	#render() {
 		const templateElem = document.createElement('template');
 		templateElem.innerHTML = template;
 		this.shadowRoot.appendChild(templateElem.content.cloneNode(true));
+		this.#link = this.shadowRoot.querySelector('.link');
 	}
 }
--- src/components/link/link-component.stories.js
+++ src/components/link/link-component.stories.js
@@ -7,15 +7,22 @@ import '../common.css';
 export default {
 	title: 'LinkComponent',
 	tags: ['autodocs'],
+	argTypes: {
+		onClick: {action: 'clicked'},
+		Text: {control: 'text'},
+		href: {control: 'text'},
+	},
 };
 
-const Template = () => {
+const Template = (args) => {
+	document.addEventListener(events.LINK_CLICKED, action(events.LINK_CLICKED));
 	return html`
-		<link-component></link-component>
+		<link-component text='${args.linkText}' is-active='${args.isActive}' href='${args.href}'></link-component>
 	`;
 };
 
 export const Default = Template.bind({});
 Default.args = {
-	variant: 'primary',
+	linkText: 'item text',
+	href: '#',
 };
