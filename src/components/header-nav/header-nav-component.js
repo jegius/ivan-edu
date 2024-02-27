ceaabb0c1591e6c2e236c3cda5c96ea5b4c83ede
Jhon-Crow <jhamish@mail.ru>
2024-02-27 20:01:35 +0300

    закомментил айтемы в темплейте, добавил слот
    начал редактировать компонент, добавил карту листенеров

--- src/components/header-nav/header-nav-component.js
+++ src/components/header-nav/header-nav-component.js
@@ -1,6 +1,29 @@
 import template from './header-nav-component.template.js';
+import {addListeners, select} from '../api/helpers';
+import events from '../api/events';
 
 export class HeaderNavComponent extends HTMLElement {
+	#slot;
+	#list;
+	#listeners = [
+		[
+			select.bind(this),
+			events.LINK_CLICKED,
+			this.#subscribeOneLinkClick.bind(this),
+		],
+		[
+			select.bind(this, 'slot'),
+			events.ON_SLOT_CHANGE,
+			this.#onSlotChange.bind(this),
+		],
+		[
+			select.bind(this, '_scrollable', window.document),
+			events_SCROLL,
+			debounce(this.#compareSectionPosition.bind(this)),
+		],
+	];
+	#linksToSelections;
+
 	constructor() {
 		super();
 		this.attachShadow({mode: 'open'});
@@ -12,11 +35,16 @@ export class HeaderNavComponent extends HTMLElement {
 
 	connectedCallback() {
 		this.#render();
+		this.#listeners.forEach(addListeners);
 	}
 
 	#render() {
 		const templateElem = document.createElement('template');
 		templateElem.innerHTML = template;
-		this.shadowRoot.appendChild(templateElem.content.cloneNode(true));
+
+
+		this.shadowRoot.append(templateElem.content.cloneNode(true));
+		this.#slot = this.shadowRoot.querySelector('slot');
+		this.#list = this.shadowRoot.querySelector('navigation');
 	}
 }
--- src/components/header-nav/header-nav-component.template.js
+++ src/components/header-nav/header-nav-component.template.js
@@ -3,12 +3,13 @@ import styles from './header-nav-component.styles.js';
 
 export default `
     ${styles}
-    <nav>
-        <ul class='header__nav'>
-            <li class='header__nav-item'><link-component></link-component></li>
-            <li class='header__nav-item'><link-component></link-component></li>
-            <li class='header__nav-item'><link-component></link-component></li>
-            <li class='header__nav-item'><link-component></link-component></li>
-        </ul>
+    <nav class='navigation'>
+      <slot></slot>
+<!--        <ul class='header__nav'>-->
+<!--            <li class='header__nav-item'><link-component></link-component></li>-->
+<!--            <li class='header__nav-item'><link-component></link-component></li>-->
+<!--            <li class='header__nav-item'><link-component></link-component></li>-->
+<!--            <li class='header__nav-item'><link-component></link-component></li>-->
+<!--        </ul>-->
     </nav>
 `;
